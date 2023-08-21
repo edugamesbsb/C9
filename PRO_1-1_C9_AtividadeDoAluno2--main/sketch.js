c74fd8dd3ae4420020806a6da4b7ff099e7b61d9
edugamesbsb <142839836+edugamesbsb@users.noreply.github.com>
2023-08-21 16:44:59 -0300

    Add files via upload

---
+++ PRO_1-1_C9_AtividadeDoAluno2--main/sketch.js
@@ -0,0 +1,37 @@
+var bola 
+
+function setup() {
+  createCanvas(400,400);
+  bola = createSprite(200, 200, 20, 20)
+
+
+
+}
+
+function draw() 
+{
+  background(30);
+
+if(keyIsDown(UP_ARROW)){
+  bola.position.y = bola.position.y -5
+}
+if(keyIsDown(DOWN_ARROW)){
+  bola.position.y = bola.position.y +5
+}
+if(keyIsDown(LEFT_ARROW)){
+  bola.position.x = bola.position.x -5
+}
+if(keyIsDown(RIGHT_ARROW)){
+  bola.position.x = bola.position.x +5
+}
+
+
+
+
+  drawSprites();
+
+}
+
+
+
+
